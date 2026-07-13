2454b31a0afaf44efbf2aabef2d380a4feafb9cd
BouchardMehdi <bouchardmehdi35@gmail.com>
2026-07-13 11:40:11 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-const fs = require('fs');
 const path = require('path');
 
-const rootDir = __dirname;
-const backDir = path.join(rootDir, 'back');
-const rootEntry = path.join(rootDir, 'dist', 'main.js');
-const backEntry = path.join(backDir, 'dist', 'main.js');
-
-if (fs.existsSync(backEntry)) {
-  process.chdir(backDir);
-  require(backEntry);
-} else if (fs.existsSync(rootEntry)) {
-  require(rootEntry);
-} else {
-  throw new Error('Build introuvable: impossible de trouver dist/main.js ou back/dist/main.js.');
-}
+process.chdir(path.join(__dirname, 'back'));
+require('./back/dist/main.js');

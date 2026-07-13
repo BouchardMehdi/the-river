a72c7543e275617f24c1eef4d35d25810c04c685
BouchardMehdi <bouchardmehdi35@gmail.com>
2026-07-13 15:58:02 +0200

    update package.json et server.js

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const path = require('path');
+const fs = require('fs');
 
-process.chdir(path.join(__dirname, 'back'));
-require('./back/dist/main.js');
+const backEntry = path.join(__dirname, 'back', 'dist', 'main.js');
+const rootEntry = path.join(__dirname, 'dist', 'main.js');
+
+if (fs.existsSync(backEntry)) {
+  process.chdir(path.join(__dirname, 'back'));
+  require(backEntry);
+} else if (fs.existsSync(rootEntry)) {
+  require(rootEntry);
+} else {
+  throw new Error('Aucun point d entree NestJS trouve: back/dist/main.js ou dist/main.js.');
+}
